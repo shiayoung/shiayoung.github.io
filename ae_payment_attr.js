ce1e48d650fa3b6f590f8edde07f81ce3053d3ea
shiayoungjh <shiayoungjh@outlook.com>
2019-11-01 10:44:11 +0800

    add ae_payment_attr.js

---
+++ ae_payment_attr.js
@@ -0,0 +1,15 @@
+﻿; (function () {
+    window.ae_payment_attr = {
+        "1": { "payApps": "" },
+        "2": { "payApps": "微信" },
+        "3": { "payApps": "支付宝" },
+        "4": { "payApps": "" },
+        "5": { "payApps": "" },
+        "200": { "payApps": "微信" },
+        "201": { "payApps": "微信" },
+        "300": { "payApps": "支付宝" },
+        "301": { "payApps": "支付宝" },
+        "600": { "payApps": "支持 USDT 转账的钱包" },
+        "700": { "payApps": "微信、支付宝、银联云闪付等" },
+    };
+})();
